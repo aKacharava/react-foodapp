e400ff36313516d3018d5c32772637b262a731fb
Alexander Kacharava <alexander@muze.nl>
2023-01-25 14:11:10 +0100

    Setup card provider context

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from './HeaderCartComponent.module.css'
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-content";
 
 const HeaderCartButton = function(props) {
+  const cartCtx = useContext(CartContext);
+  const amountOfCartItems = cartCtx.items.reduce((currentAmount, item) => {
+    return currentAmount + item.amount;
+  }, 0)
+
   return (
     <button className={classes.button} onClick={props.onClickButton}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{amountOfCartItems}</span>
     </button>
   );
 }
