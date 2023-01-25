ea7bc1bff9527f38c0fa4d0fa5931a7746549c14
Alexander Kacharava <alexander@muze.nl>
2023-01-25 14:42:14 +0100

    Add amount of items added functionality

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,20 +1,47 @@
-import classes from './MealItemForm.module.css'
-import Input from '../../UI/Input'
+import { useRef, useState } from 'react';
+import classes from './MealItemForm.module.css';
+import Input from '../../UI/Input';
 
 const MealItemForm = function(props) {
-  return(
-    <form className={classes.form}>
-      <Input label="Amount" input={{
-        id: "amount_" + props.id,
-        type: "number",
-        min: "1",
-        max: "5",
-        step: "1",
-        defaultValue: "1"
-      }} />
+  const [isAmountValid, setIsAmountValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = event => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const convertedAmount = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      convertedAmount < 1 ||
+      convertedAmount > 5
+    ) {
+      setIsAmountValid(false);
+      return;
+    }
+
+    props.onAddToCart(convertedAmount);
+  }
+
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <Input
+        ref={amountInputRef}
+        label="Amount"
+        input={{
+          id: "amount_" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
       <button>+ Add</button>
+      {!isAmountValid && <p>Please enter a valid amount between 1-5.</p>}
     </form>
-  )
+  );
 }
 
 export default MealItemForm
